020845c215db7ec5d3801b7125ccfbb08703ba95
Chowiya <nd1553410@gmail.com>
2024-12-28 18:55:08 +0530

    using get method

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 // DSA ARRAYS
 
-// push method
+
 
 class myFruits{
     constructor(){
         this.length="0";
         this.data={};
     }
-
+// push method
     push(items){
       this.data[this.length]=items
       this.length++
       return this.length
     }
+// get method
+get(index){
+    return this.data[index]
+} 
 }
 
 const myList = new myFruits();
 myList.push("Apple");
 myList.push("banana");
 myList.push("mango"); 
-console.log(myList);
+// get method 
+console.log(myList.get(1));
